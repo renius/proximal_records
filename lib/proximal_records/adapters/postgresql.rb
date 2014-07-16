824ae8d793a5203303e45123d6a566a6e0574a1d
renius <antiqe@gmail.com>
2014-07-16 07:22:44 +0400

    Update postgresql.rb
    circle feature

--- lib/proximal_records/adapters/postgresql.rb
+++ lib/proximal_records/adapters/postgresql.rb
@@ -14,7 +14,17 @@ module ProximalRecords
         as = table.as(Arel.sql('z'))
         a = klass.select('z.*').from(as.to_sql).where(z: {klass.primary_key => id}).limit(1)[0]
 
-        [(klass.find_by_id(a.previous_id)), (klass.find_by_id(a.next_id))]
+        previous_record, next_record = [(klass.find_by_id(a.previous_id)), (klass.find_by_id(a.next_id))]
+
+        case scope.count
+          when 1
+            [nil, nil]
+          when 2
+            [next_record || previous_record] * 2
+          else
+            [previous_record || scope.last, next_record || scope.first]
+        end
+        
       end
     end
   end
